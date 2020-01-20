abd4c45dea5cd3e148f5215a223c6305b26013f5
cmruss <cmrussell08@gmail.com>
2020-01-19 23:14:46 -0500

    Router, CRUD implemented, SQL queries solutions

---
+++ accounts/account-router.js
@@ -0,0 +1,87 @@
+const express = require('express');
+
+const knex = require('../data/dbConfig');
+
+const router = express.Router();
+
+router.get('/', (req, res) => {
+
+    knex
+        .select('*')
+        .from('accounts')
+        .then(accounts => {
+            res.status(200).json(accounts);
+        })
+        .catch(error => {
+            console.log(error)
+            res.status(500).json({ errorMessage: "Error retrieving accounts." })
+        });
+});
+
+router.get('/:id', (req, res) => {
+
+    knex
+        .select('*')
+        .from('accounts')
+        .where({ id: req.params.id })
+        .first()
+        .then(account => {
+            res.status(200).json(account)
+        })
+        .catch(error => {
+            console.log(error)
+            res.status(500).json({ errorMessage: "Error obtaining account." })
+        });
+});
+
+router.post('/', (req, res) => {
+    
+    knex('accounts')
+        .insert(req.body, 'id')
+        .then(ids => {
+            const id = ids[0]
+            return knex('accounts')
+            .where({ id })
+            .first()
+            .then(post => {
+                res.status(201).json(post)
+            })
+        })
+        .catch(error => {
+            console.log(error)
+            res.status(500).json({ errorMessage: "Error adding account."})
+        });
+});
+
+router.put('/:id', (req, res) => {
+    const { id } = req.params;
+    const changes = req.body;
+
+    knex('accounts')
+    .where({id})
+    .update(changes)
+    .then(count => {
+        if (count > 0)
+        res.status(201).json({ message: `${count} account updated.` })
+    })
+    .catch(error => {
+        console.log(error);
+        res.status(404).json({ errorMessage: "Error modifying account."})
+    });
+});
+
+router.delete('/:id', (req, res) => {
+    const { id } = req.params;
+    knex('accounts')
+        .where({ id })
+        .delete()
+        .then(count => {
+            res.status(200).json({ message: `${count} account deleted.`})
+        })
+        .catch(error => {
+            console.log(error);
+            res.status(500).json({ errorMessage: "Unable to delete post."})
+        })
+})
+
+module.exports = router;
